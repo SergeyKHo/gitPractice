56d701985b7f5f55ee143c5963a018b9c111088b
Sergey <myphone.kg2@mail.ru>
2023-10-11 17:36:17 +0300

    addNewTest111023

--- src/test/java/org/example/models/TextBoxTest.java
+++ src/test/java/org/example/models/TextBoxTest.java
@@ -1,31 +1,36 @@
 package org.example.models;
 
 import org.example.driver.Driver;
+import org.example.models.pages.TextBoxPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class TextBoxTest extends BaseTest {
+public class CeckTextBox extends BaseTest{
 
-    WebDriver driver;
-    @BeforeClass
-    public void setUp()  {
-        driver = Driver.getDriver();
-    }
-    @AfterClass
-    public void tearDown() {
-        driver.close();
-        driver.quit();
-    }
-
-
+//    WebDriver driver;
+//    @BeforeClass
+//    public void setUp()  {
+//        driver = Driver.getDriver();
+//    }
+//    @AfterClass
+//    public void tearDown() {
+//        driver.close();
+//        driver.quit();
+//    }
 
     @Test
-    void fillUpTheFormTest(){
-        driver.get("https://demoqa.com/text-box");
-        Student student =new Student();
-        textBoxPage.fillUpTheForm(student);
-    }
+    void test () {
+        driver.get("https://demoqa.com/text-box/");
+        TextBoxPage textBoxPage = new TextBoxPage();
+        textBoxPage.fillUpFieldName("Ann")
+                .fillUpFieldEmail("ann@gamil.com")
+                .fillUpFieldCAddress("Asanbai")
+                .fillUpFieldPAddress("Bishkek")
+                .submitBtmClick().testTexBoxEquals();
 
+
+
+    }
 }
